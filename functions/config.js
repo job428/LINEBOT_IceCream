b590e4191fc26f2e52705607ccb19726c8b0ccb7
job428 <chaimala335@gmail.com>
2022-05-01 17:08:18 +0700

    new commit

--- functions/config.js
+++ functions/config.js
@@ -1,7 +1,7 @@
 const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message';
 const LINE_HEADER = {
   'Content-Type': 'application/json',
-  'Authorization': `Bearer 5pRqQxXEt7Cx0cjgIbpIQ64on0sY5CgJn1BMAXmxpdkSzDVc6WgoJ7Zg20bqIvxVqcNlIJ23/UebRyOZzRXenzUQ1sp0AjR/kZd2T8MjmWJrvfYNujkYskG3CDBfQnU4ow6Eqtpf3IAYfb02j/l61QdB04t89/1O/w1cDnyilFU=`
+  'Authorization': `Bearer qjQZKe5UNjl1imFopuJ5yl6DDfel3sMK9BsHQUYQ/O2Bxe8AGgG8YxBT4S65RMGfKh6Y+O4Vyo6MproNzTdQywWkCKPsGvnOWrk4LpSxn/4GwYZWvVuzzXIcClAtVP/IPCrigDKuO1YlDtt6mYk3XgdB04t89/1O/w1cDnyilFU=`
 };
 
 module.exports = {LINE_MESSAGING_API,LINE_HEADER}
